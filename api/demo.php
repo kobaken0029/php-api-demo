be3bca7d6326de17be3195186f795bef5153b683
kento_kobayashi <kobaken0029@gmail.com>
2016-04-18 01:22:13 +0900

    クールに対応するように修正＆ちょっとリファクタ

--- api/demo.php
+++ api/demo.php
@@ -1,24 +1,48 @@
 <?php
+
+/**
+ * 対象のURLをCURLで叩いた結果を返す。
+ *
+ * @param string $target_url APIのURL
+ * @return array APIの結果
+ */
+public function createResult($target_url)
+{
+    // curlオブジェクト初期化
+    $curl = curl_init();
+
+    // オプションを設定
+    $option = [
+        CURLOPT_URL => $target_url,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_RETURNTRANSFER => true
+    ];
+    curl_setopt_array($curl, $option);
+
+    // 結果をjsonデコード
+    $result = json_decode(curl_exec($curl), true);
+
+    // curlを閉じる
+    curl_close($curl);
+
+    // 結果がnullなら空配列を返却
+    return $result ?: [];
+}
+
 header('Access-Control-Allow-Origin:*');
 header("Content-Type: application/json");
 
-$base_url = 'http://api.moemoe.tokyo/anime';
+$base_url = 'http://api.moemoe.tokyo/anime/v1';
+$target_url = $base_url.'/master';
 
-$year = 2016;
 if (isset($_GET['year']) && is_numeric($_GET['year'])) {
     $year = $_GET['year'];
+    $target_url .= '/'.$year;
+
+    if (isset($_GET['cour']) && is_numeric($_GET['cour'])) {
+        $cour = $_GET['cour'];
+        $target_url .= '/'.$cour;
+    }
 }
 
-$curl = curl_init();
-$option = [
-    CURLOPT_URL => $base_url.'/v1/master/'.$year,
-    CURLOPT_SSL_VERIFYPEER => false,
-    CURLOPT_RETURNTRANSFER => true
-];
-curl_setopt_array($curl, $option);
-
-$response = curl_exec($curl);
-$result = json_decode($response, true);
-curl_close($curl);
-
-echo $result ?: [];
+echo createResult($target_url);
